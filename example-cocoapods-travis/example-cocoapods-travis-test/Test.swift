211056c83c1d7758bf05dd758b51b5fcb1f029fe
Nicholas Ver Voort <nvervoort@blackducksoftware.com>
2019-07-22 14:47:32 -0400

    Initial commit of working swift project and test

---
+++ example-cocoapods-travis/example-cocoapods-travis-test/Test.swift
@@ -0,0 +1,17 @@
+//
+//  Test.swift
+//  example-cocoapods-travis-test
+//
+//  Created by Nick Ver Voort on 7/22/19.
+//  Copyright © 2019 Black Duck Copilot. All rights reserved.
+//
+
+import XCTest
+
+class Test: XCTestCase {
+
+    func testExample() {
+        XCTAssert(true);
+    }
+
+}
---
+++ example-cocoapods-travis/example-cocoapods-travis-test/example_cocoapods_travis_test.swift
@@ -0,0 +1,33 @@
+//
+//  example_cocoapods_travis_test.swift
+//  example-cocoapods-travis-test
+//
+//  Created by Nick Ver Voort on 7/22/19.
+//  Copyright © 2019 Black Duck Copilot. All rights reserved.
+//
+
+import XCTest
+
+class example_cocoapods_travis_test: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
